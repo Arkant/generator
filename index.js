66aedf4ac8ff03bb26ded5e1da346357e0f64a3e
Arkant <pan_rostislav@ukr.net>
2019-01-15 16:04:31 +0200

    fixed async problem

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-function sum(a, b) {
-    return a + b;
+function sum() {
+  console.log(1);
+  return [].reduce.call(arguments, (acc, el) => acc+=el);
 }
-var num = 20;
-var obj = {name: 'ivan'}
-var prom = new Promise( res => {
-  setTimeout(res, 1000, 10);
-});
+
+const prom = x => new Promise(res => {
+  console.log(2);
+  setTimeout(res,2000,x);
+})
+
+function pow() {
+  console.log(3);
+  return [].reduce.call(arguments, (acc, el) => acc*=el);
+}
+
+const arr = [1,2,3,4]
 
 function *gen() {
-  const a = yield () => sum(1,2);
-  const b = yield prom;
-  const c = yield num;
-  const d = yield obj;
-  console.log('a', a)
-  console.log('b', b)
-  console.log('c', c)
-  console.log('d', d)
+  const a = yield sum.bind(null, ...arr);
+  const b = yield prom(a);
+  const c = yield pow.bind(null, ...arr);
+  const d = yield arr;
+  console.log(a + b + c + d)
+  yield a + b + c + d;
 }
 
 const iterator = gen();
 
 function check (item, array) {
     if (item instanceof Promise) {
-      return item.then(data => {
+      item.then(data => {
         array.push(data);
         return data
       });
     } else if (typeof item === 'function') {
         array.push(item());
         return item()
-    } else if (typeof item === 'undefined'){
+    } else if (typeof item === 'undefined') {
         return;
     } else {
       array.push(item);
@@ -48,10 +54,10 @@ async function runner(iterat) {
     await value;
     next = iterat.next(result);
     done = next.done;
-    value = next.value;
+    value = await next.value;
     result = check(value, array);
   }
   return array;
 }
 
-runner(iterator).then(data => console.log(data));
+runner(iterator).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"))
